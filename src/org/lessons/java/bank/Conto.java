aba7ae3eb2d60066a8f25e866bb9a840f9e310b3
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-20 16:47:01 +0200

    add constructor for Conto

--- src/org/lessons/java/bank/Conto.java
+++ src/org/lessons/java/bank/Conto.java
@@ -1,8 +1,19 @@
 package org.lessons.java.bank;
 
+import java.util.Random;
+
 public class Conto {
     // ARGOMENTI
     private int numeroConto;
     private String nomeCliente;
     private double saldo;
+
+    //COSTRUTTORI
+    public Conto(String nomeCliente) {
+        Random random = new Random();
+        numeroConto = random.nextInt(100000000);
+        this.nomeCliente = nomeCliente;
+        saldo = 0.00;
+    }
+
 }
