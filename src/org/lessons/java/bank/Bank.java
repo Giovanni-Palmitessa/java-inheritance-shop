1a7108752981ecab9c5401fa42ed79e1feb45a9b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-20 17:04:33 +0200

    add scanner

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -1,7 +1,13 @@
 package org.lessons.java.bank;
 
+import java.util.Scanner;
+
 public class Bank {
     public static void main(String[] args) {
+        //inizializzare lo scanner
+        Scanner scan = new Scanner(System.in);
 
+        //chiudere lo scanner
+        scan.close();
     }
 }
