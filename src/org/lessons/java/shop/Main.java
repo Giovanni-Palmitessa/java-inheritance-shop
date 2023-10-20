e67ae3d10789eb72295dca4d88cf160af77edcc1
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-20 15:45:55 +0200

    create new product

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,7 +2,7 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto prodotto1 = new Prodotto("Prodotto 1", "Descrizione 1", 10.0, 22);
+        Prodotto prodotto1 = new Prodotto("iPhone 15", "L'iPhone più potente di sempre", 1500.0, 22);
         System.out.println(prodotto1.getCodice());
     }
 }
