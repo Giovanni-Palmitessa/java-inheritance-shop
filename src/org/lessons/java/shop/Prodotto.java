b8d081afb8e8d3c3eb6c17c13f708a090defa8ed
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-20 14:35:26 +0200

    Add attributes

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,4 +1,10 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+    //ATTRIBUTI
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private int iva;
 }
