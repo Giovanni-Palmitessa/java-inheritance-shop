c23755ad5fb064f8f32f4e3c2e15b1c9f06479ff
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-20 16:57:47 +0200

    add methods preleva and deposita

--- src/org/lessons/java/bank/Conto.java
+++ src/org/lessons/java/bank/Conto.java
@@ -33,4 +33,22 @@ public class Conto {
     public void setNomeCliente(){
         this.nomeCliente = nomeCliente;
     }
+
+    // METODI
+
+    // metodo per aggiungere soldi al saldo
+    public double deposita(double money) {
+        saldo += money;
+        return saldo;
+    }
+
+    // metodo per prelevare soldi al saldo
+    public double preleva(double money) {
+        if (money <= saldo) {
+            saldo -= money;
+        } else {
+            System.out.println("Saldo insufficiente per il prelievo.");
+        }
+        return saldo;
+    }
 }
