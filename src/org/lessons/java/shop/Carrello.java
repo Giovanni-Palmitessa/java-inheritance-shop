00d8850d8892c2dde962891917967f2335e3692a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-23 15:00:58 +0200

    add print of choises for insert product

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -1,7 +1,15 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Carrello {
     public static void main(String[] args) {
+        //istanzio lo scanner
+        Scanner scan = new Scanner(System.in);
 
+        //Ciclo while per inserire un prodotto
+        while (true) {
+            System.out.println("Inserisci il tipo di prodotto (1 = Smartphone, 2 = TV, 3 = Cuffie, 0 = Esci): ");
+        }
     }
 }
