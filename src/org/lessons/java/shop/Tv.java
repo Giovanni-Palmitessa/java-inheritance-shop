9d990ceaa881e6f24f41739596d83672b1115db3
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-23 14:35:50 +0200

    add attributes dimension and isSmartTv and constructor

---
+++ src/org/lessons/java/shop/Tv.java
@@ -0,0 +1,14 @@
+package org.lessons.java.shop;
+
+public class Tv extends Prodotto{
+    //ATTRIBUTI
+    private int dimension;
+    private boolean isSmartTv;
+
+    public Tv(String nome, String descrizione, double prezzo, int iva) {
+        super(nome, descrizione, prezzo, iva);
+    }
+
+    //COSTRUTTORE
+
+}
