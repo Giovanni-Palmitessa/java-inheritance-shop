1de8177ec4310c9e762d65b4e8c9936328769204
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-23 17:18:31 +0200

    add method to string for TV

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -77,7 +77,7 @@ public class Carrello {
         //stampare resoconto carrello
         System.out.println("Carrello:");
         for (int i = 0; i < indice; i++) {
-            System.out.println(indice + " - " + carrello[i].toString());
+            System.out.println(i + 1 + " - " + carrello[i].toString());
         }
 
         System.out.println("Totale: " + totale);
